555d88770899c7bff40dd2c708aa6064f8755d40
delmas007 <azertyazerty1ze@gmail.com>
2025-08-01 07:50:54 +0000

    oauth

--- customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/web/CustomerController.java
+++ customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/web/CustomerController.java
@@ -2,9 +2,12 @@ package ci.delmas.customerfrontthymeleaftapp.web;
 
 import ci.delmas.customerfrontthymeleaftapp.model.Customer;
 import ci.delmas.customerfrontthymeleaftapp.repository.CustomerRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
@@ -14,6 +17,18 @@ public class CustomerController {
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/auth")
+    @ResponseBody
+    public Authentication authentication(Authentication authentication) {
+        return authentication;
+    }
+
 
     @GetMapping("/customers")
     public String customers(Model model) {
