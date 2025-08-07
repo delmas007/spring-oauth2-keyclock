5b71b87f3148a4ff0f511bcc0c843846ec24000c
delmas007 <azertyazerty1ze@gmail.com>
2025-08-07 19:22:04 +0000

    oauth

--- customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/sec/SecurityConfig.java
+++ customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/sec/SecurityConfig.java
@@ -19,7 +19,14 @@ import java.util.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
 
 
     @Bean
@@ -28,13 +35,46 @@ public class SecurityConfig {
                 .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(ar->ar.requestMatchers("/","/oauth2Login/**","/webjars/**","/h2-console/**").permitAll())
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(al->
+                        al.loginPage("/oauth2Login")
+                                .defaultSuccessUrl("/")
+                )
                 .logout((logout) -> logout
-//                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         .logoutSuccessUrl("/").permitAll()
                         .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"))
                 .exceptionHandling(eh->eh.accessDeniedPage("/notAutorized"))
                 .build();
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        final OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}?logoutsuccess=true");
+        return oidcLogoutSuccessHandler;
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            final Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            authorities.forEach((authority) -> {
+                if (authority instanceof OidcUserAuthority oidcAuth) {
+                    mappedAuthorities.addAll(mapAuthorities(oidcAuth.getIdToken().getClaims()));
+                    System.out.println(oidcAuth.getAttributes());
+                } else if (authority instanceof OAuth2UserAuthority oauth2Auth) {
+                    mappedAuthorities.addAll(mapAuthorities(oauth2Auth.getAttributes()));
+                }
+            });
+            return mappedAuthorities;
+        };
+    }
+    private List<SimpleGrantedAuthority> mapAuthorities(final Map<String, Object> attributes) {
+        final Map<String, Object> realmAccess = ((Map<String, Object>)attributes.getOrDefault("realm_access", Collections.emptyMap()));
+        final Collection<String> roles = ((Collection<String>)realmAccess.getOrDefault("roles", Collections.emptyList()));
+        return roles.stream()
+                .map((role) -> new SimpleGrantedAuthority(role))
+                .toList();
     }
 }
--- customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/web/CustomerController.java
+++ customer-front-thymeleaft-app/src/main/java/ci/delmas/customerfrontthymeleaftapp/web/CustomerController.java
@@ -2,19 +2,26 @@ package ci.delmas.customerfrontthymeleaftapp.web;
 
 import ci.delmas.customerfrontthymeleaftapp.model.Customer;
 import ci.delmas.customerfrontthymeleaftapp.repository.CustomerRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CustomerController {
     private final CustomerRepository customerRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
         this.customerRepository = customerRepository;
     }
 
@@ -31,9 +38,28 @@ public class CustomerController {
 
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String customers(Model model) {
         List<Customer> customerList = customerRepository.findAll();
         model.addAttribute("customers", customerList);
         return "customers";
+    }
+
+    @GetMapping("/notAutorized")
+    public String notAutorized() {
+        return "notAuthorized";
+    }
+
+    @GetMapping("/oauth2Login")
+    public String oauth2Login(Model model) {
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
     }
 }
