2544b4a7992d36536291b242f17c9e1037090727
delmas007 <azertyazerty1ze@gmail.com>
2025-08-09 02:23:16 +0000

    guards

--- customer-front-angular-app/src/app/app.config.ts
+++ customer-front-angular-app/src/app/app.config.ts
@@ -1,14 +1,44 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {KeycloakService} from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    KeycloakService,
     provideHttpClient()
   ]
 };
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'test',
+        clientId: 'front-angular'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        checkLoginIframe : true,
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
